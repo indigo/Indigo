01728f5b10d98356f9dcb3155a61a9c3cdfcfb47
Richard Rispoli <rrispoli@hibernum.com>
2016-04-06 20:35:39 -0400

    first try

---
+++ NewHope/Assets/Scripts/World/Blocks/Water.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water :Block
+{
+		public Water ()
+		{
+				this.alpha = true;
+				this.uvBase = new Vector2 (0, Random.Range (6, 8));
+		}
+		protected override void OnUpdate (World world, int x, int y, int z)
+		{
+				if (world.GetBlock (x, y, z) == null) {
+						world.SetBlock (new Water (), x, y, z);
+				}
+		}
+}
