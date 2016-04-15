4ac42e5803a40be36e31f69cafe9c72e5d8cb744
Richard Rispoli <rrispoli@hibernum.com>
2016-04-14 20:44:00 -0400

    jsp

--- PuzzleX/Assets/TileFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public static class TileFactory {
-    public static Tile GetTile() {
-        Tile t = Tile.CreateTile();
-        return t;
-    }
-}
